ccd025d403e7e9f44fa7cf9db984da65073c161d
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-01-19 14:22:56 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # photo_processing
+Teeny repo for renaming photos by putting datestamp and camera model in name, and/or adding GPS coordinates to image metadata using exiftool
---
+++ scripts/image_processing.R
@@ -0,0 +1,33 @@
+
+
+# GPS coordinate addition function ----------------------------------------
+
+coord_function <- function(coord_location, image_in_location, 
+                           image_out_location) {
+
+}
+
+
+# Renaming function -------------------------------------------------------
+
+rename_function <- function(image_in_location, image_out_location) {
+  # add time and date
+
+  # add device model or ID
+}
+
+
+
+
+
+
+# Function which the user calls -------------------------------------------
+
+user_function <- function(add_coords, rename_files, coord_dir, image_dir) {
+  if (add_coords == T) {
+
+  }
+  if (rename_files == T) {
+
+  }
+}
